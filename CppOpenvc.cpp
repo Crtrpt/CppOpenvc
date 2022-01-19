9d48d734684b8a8aa2cd4f3b27023d483636a88f
jzx <945627077@qq.com>
2022-01-19 11:10:27 +0800

    '格式化代码'

--- CppOpenvc.cpp
+++ CppOpenvc.cpp
@@ -19,39 +19,32 @@ CascadeClassifier eyes_cascade;
 
 int main()
 {
-	
+
 	cout << "start openstart";
 	String face_cascade_name = samples::findFile("C:\\Users\\Admin\\source\\repos\\CppOpenvc\\lbpcascades\\lbpcascade_frontalface.xml");
 	auto cls = face_cascade.load(face_cascade_name);
 
 	auto img = imread("C:\\Users\\Admin\\source\\repos\\CppOpenvc\\test.png", 0);
 
+	vector<Rect> faces;
+	face_cascade.detectMultiScale(img, faces, 1.1, 3, 0);
+	cout << faces.size();
+	cout << "find";
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		Point  center;
+		int radius;
+		center.x = cvRound((faces[i].x + faces[i].width * 0.5));
+		center.y = cvRound((faces[i].y + faces[i].height * 0.5));
+		radius = cvRound((faces[i].width + faces[i].height) * 0.25);
+		circle(img, center, radius, CV_RGB(255, 0, 0), 2);
+	}
+	double scale = 0.5;
+	Size dsize = Size(img.cols * scale, img.rows * scale);
 
-	    Mat frame;
-	
-	   /* Mat frame_gray;*/
-	    //-- Detect faces
-		vector<Rect> faces;
-	    face_cascade.detectMultiScale(img, faces,1.1,3,0);
-		cout << faces.size();
-		cout << "find";
-	    for ( size_t i = 0; i < faces.size(); i++ )
-	    {
-			Point  center;
-			int radius;
-			center.x = cvRound((faces[i].x + faces[i].width * 0.5));
-			center.y = cvRound((faces[i].y + faces[i].height * 0.5));
-			radius = cvRound((faces[i].width + faces[i].height) * 0.25);
-			circle(img, center, radius, CV_RGB(255, 0, 0), 2);
-	    }
-		double scale = 0.5;
-		Size dsize = Size(img.cols * scale, img.rows * scale);
+	resize(img, img, dsize);
 
-		resize(img, img,dsize );
-
-		cout << "==============";
-	    //-- Show what you got
-	   imshow( "人脸识别", img);
-	   cv::waitKey();
+	imshow("人脸识别", img);
+	cv::waitKey();
 	return 0;
 }
